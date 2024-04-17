b1d65af2cb68cd60e980c8dc6099eeec87496dc1
eunju <eun.juuu0@gmail.com>
2024-04-18 00:05:41 +0900

    set: first commit

---
+++ src/constants/config.ts
@@ -0,0 +1,17 @@
+// Config file for your website
+export const SiteConfig = {
+  url: "https://sionic.ai",
+  pathPrefix: "/",
+  title: "사이오닉 에이아이 - 기업용 AI-Native 솔루션",
+  description:
+    "생성형 AI를 기술적 고민 없이 기업에 바로 적용할 수 있도록 하는 솔루션을 제공합니다. 사이오닉 에이아이와 함께 미래를 대비하고 비즈니스 경쟁력을 확보하세요.",
+  subtitle: "Sionic AI",
+  copyright: "Sionic AI © All rights reserved.",
+  author: {
+    name: "eunchu",
+    photo: "/profile.png",
+    contacts: {
+      email: "eun.juuu0@gmail.com",
+    },
+  },
+};
