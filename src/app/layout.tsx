8f8ba8cbd0605c8f373dbed3f9ae60b77194d39f
eunju <eun.juuu0@gmail.com>
2024-04-18 00:25:59 +0900

    test: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,15 +10,27 @@ export const metadata: Metadata = {
   title: "App Title",
   description: "Description of the app",
   openGraph: {
+    title: "App Title",
+    description: "Description of the app",
     type: "website",
     locale: "ko_KR",
-    url: "https://nextjs14-test-nine.vercel.app/",
+    url: "https://nextjs14-test-nine.vercel.app",
     images: {
-      url: ogImage.src,
+      url: "/openGraph-img.jpeg",
       width: ogImage.width,
       height: ogImage.height,
     },
   },
+  twitter: {
+    title: "App Title",
+    description: "Description of the app",
+    images: {
+      url: "/openGraph-img.jpeg",
+      width: ogImage.width,
+      height: ogImage.height,
+    },
+  },
+  metadataBase: new URL("https://nextjs14-test-nine.vercel.app"),
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon.ico",
