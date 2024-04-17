d39f480c8c4a3f281c2bce5a4ad6a8202085ac8d
eunju <eun.juuu0@gmail.com>
2024-04-18 00:28:32 +0900

    fix: ogImage 경로수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import ogImage from "./openGraph-img.jpeg";
+import ogImage from "../../public/openGraph-img.jpeg";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
